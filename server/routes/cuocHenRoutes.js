785eb30f01279089274d53327c9c2cea17610c7b
hoanhhop <21107481.hop@student.iuh.edu.vn>
2025-11-18 20:04:54 +0700

    fix(routes): map cuocHenRoutes to correct CuocHenController methods
    - Fix Route.post() requires callback error
    - Update routes to use class-based controller methods
    - Map: layDanhSachCuocHen, xacNhanCuocHen, pheDuyetCuocHen, tuChoiCuocHen
    - Remove old CRUD methods (create, getAll, findBy*, update, delete)
    
    Resolves: Server startup error after upstream merge
    Refs: v1.3.0-merge-upstream

--- server/routes/cuocHenRoutes.js
+++ server/routes/cuocHenRoutes.js
@@ -1,29 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const cuocHenCtrl = require("../controllers/cuocHenController");
+const CuocHenController = require("../controllers/cuocHenController");
 
-// Public: tạo cuộc hẹn (khách) - body bắt buộc: PhongID, KhachHangID, NhanVienBanHangID, ThoiGianHen
-router.post("/", cuocHenCtrl.create);
+// Lấy danh sách cuộc hẹn (dùng bởi Chủ dự án)
+router.get("/", CuocHenController.layDanhSachCuocHen);
 
-// Lấy tất cả cuộc hẹn
-router.get("/", cuocHenCtrl.getAll);
+// Xác nhận cuộc hẹn
+router.put("/:id/xac-nhan", CuocHenController.xacNhanCuocHen);
 
-// Tìm theo Khách hàng
-router.get("/search/khach-hang/:khachHangId", cuocHenCtrl.findByKhachHang);
+// Phê duyệt cuộc hẹn
+router.put("/:id/phe-duyet", CuocHenController.pheDuyetCuocHen);
 
-// Tìm theo Nhân viên
-router.get("/search/nhan-vien/:nhanVienId", cuocHenCtrl.findByNhanVien);
-
-// Tìm theo Chủ dự án (admin/owner view)
-router.get("/search/chu-du-an/:chuDuAnId", cuocHenCtrl.findByChuDuAn);
-
-// Lấy chi tiết cuộc hẹn
-router.get("/:id", cuocHenCtrl.getById);
-
-// Cập nhật cuộc hẹn
-router.put("/:id", cuocHenCtrl.update);
-
-// Xóa cuộc hẹn
-router.delete("/:id", cuocHenCtrl.delete);
+// Từ chối cuộc hẹn
+router.put("/:id/tu-choi", CuocHenController.tuChoiCuocHen);
 
 module.exports = router;
